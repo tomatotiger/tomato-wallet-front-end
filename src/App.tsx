03c12554d5c2013adca1127648668864a51bd8a0
Judy <yanghuanghuang@gmail.com>
2019-04-16 10:47:04 -0700

    set up enzyme, CircleCI and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
+    let a;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
