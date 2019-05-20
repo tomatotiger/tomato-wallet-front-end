66970062b2cff0c45ec1a44ef12ebd1de7e7bc42
Judy <yanghuanghuang@gmail.com>
2019-05-20 11:23:31 -0700

    connect component to store

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
-import './App.scss';
-import { RecordExpense } from './components/RecordExpense';
+import { connect } from 'react-redux';
+import { Dispatch, AnyAction } from 'redux';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
-class App extends Component {
+import { AppState } from './store';
+import { login } from './store/auth/actions';
+import { getCategories } from './store/category/actions';
+import { getExpenses } from './store/expense/actions';
+import { AuthState } from './store/auth/types';
+import { CategoryState } from './store/category/types';
+import { ExpenseHistoryState } from './store/expense/types';
+import { Homepage } from './pages/Homepage';
+import { Footer } from './components/Footer';
+import './stylesheets/main.scss';
+import { thunkGetInitialData } from './thunks';
+
+interface AppProps {
+  auth: AuthState;
+  category: CategoryState;
+  expense: ExpenseHistoryState;
+  handleInitialData: () => Promise<void>;
+}
+
+class App extends Component<AppProps> {
+  componentDidMount() {
+    this.props.handleInitialData();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">Tomato Wallet</header>
-        <div className="content">
-          <RecordExpense />
-        </div>
+      <div className="app">
+        <Homepage />
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
+const testUser: AuthState = {
+  loginState: 'LOGGED_IN',
+  uid: 1,
+  username: 'tomato'
+};
+
+export const handleInitialData = () => (dispatch: Dispatch<AnyAction>) => {
+  dispatch(showLoading());
+  dispatch(login(testUser));
+  return thunkGetInitialData(1).then(({ categories, expenses }) => {
+    dispatch(getCategories(categories));
+    dispatch(getExpenses(expenses));
+    dispatch(hideLoading());
+  });
+};
+
+const mapStateToProps = (state: AppState) => ({
+  auth: state.auth,
+  category: state.category,
+  expense: state.expense
+});
+
+export default connect(
+  mapStateToProps,
+  { handleInitialData }
+)(App);
