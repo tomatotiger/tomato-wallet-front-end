6d00e91730776cdf13ef40f4b0bb1976b87a6b75
Judy <yanghuanghuang@gmail.com>
2019-07-04 23:09:45 -0700

    implement common function for get a API which returned with new type APIResponse.

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,6 @@
+import { Result } from '../utils/result';
 const env = process.env;
+
 let APIUrl: string;
 switch (env.NODE_ENV) {
   case 'development':
@@ -9,13 +11,73 @@ switch (env.NODE_ENV) {
     break;
 }
 
-export const httpGet = <T>(
+interface NetworkError {
+  kind: 'NetworkError';
+}
+
+interface BadStatus {
+  kind: 'BadStatus';
+  response: Response;
+}
+
+interface BadBody {
+  kind: 'BadBody';
+  response: Response;
+}
+
+interface BadSchema<Err> {
+  kind: 'BadSchema';
+  error: Err;
+}
+//TODO: extra error: Timeout, BadBody, BadUrl
+
+const networkError = (): NetworkError => ({ kind: 'NetworkError' });
+const badStatus = (response: Response): BadStatus => ({
+  kind: 'BadStatus',
+  response
+});
+const badBody = (response: Response): BadBody => ({
+  kind: 'BadBody',
+  response
+});
+const badSchema = <Err>(error: Err): BadSchema<Err> => ({
+  kind: 'BadSchema',
+  error
+});
+
+type APIError<DecodeError> =
+  | NetworkError
+  | BadStatus
+  | BadBody
+  | BadSchema<DecodeError>;
+
+export type APIResponse<Data, DecodeError> = Result<
+  Data,
+  APIError<DecodeError>
+>;
+
+export const httpGet = async <Data, DecodeError>(
   endPoint: string,
-  validation: (json: any) => T
-): Promise<T> => {
-  return fetch(APIUrl + endPoint)
-    .then(resp => resp.json())
-    .then(json => {
-      return validation(json);
-    });
+  decode: (json: any) => Result<Data, DecodeError>
+): Promise<APIResponse<Data, DecodeError>> => {
+  try {
+    const resp: Response = await fetch(APIUrl + endPoint);
+    if (resp.status >= 200 && resp.status < 300) {
+      try {
+        const json: any = await resp.json();
+        const result = decode(json);
+        if (result.success) {
+          return Result.success(result.data);
+        } else {
+          return Result.failure(badSchema(result.error));
+        }
+      } catch (e) {
+        return Result.failure(badBody(resp));
+      }
+    } else {
+      return Result.failure(badStatus(resp));
+    }
+  } catch (e) {
+    return Result.failure(networkError());
+  }
 };
